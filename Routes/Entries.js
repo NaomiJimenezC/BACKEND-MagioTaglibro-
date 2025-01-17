b309501388e3eaadfd8f1ac99a27ef31811d7a8d
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2025-01-17 10:19:06 +0100

    posst hecho

--- Routes/Entries.js
+++ Routes/Entries.js
@@ -12,4 +12,34 @@ router.get("/", async (req, res) => {
     }
 });
 
+// Crear una nueva entrada
+router.post("/", async (req, res) => {
+    const { titulo, contenido, autor_id } = req.body;
+
+    // Validar los datos recibidos
+    if (!titulo || !contenido || !autor_id) {
+        return res.status(400).json({ message: "Título, contenido y autor_id son requeridos." });
+    }
+
+    // Verificar si el autor existe
+    try {
+        const usuarioExistente = await Usuario.findById(autor_id);
+        if (!usuarioExistente) {
+            return res.status(404).json({ message: "El usuario no existe." });
+        }
+
+        const nuevaEntrada = new Entrada({
+            titulo,
+            contenido,
+            autor_id,  // Usar ObjectId aquí para referenciar al usuario existente
+        });
+
+        const savedEntry = await nuevaEntrada.save();
+        res.status(201).json(savedEntry);
+
+    } catch (error) {
+        res.status(500).json({ message: "Error al guardar la entrada", error: error.message });
+    }
+});
+
 module.exports = router;
