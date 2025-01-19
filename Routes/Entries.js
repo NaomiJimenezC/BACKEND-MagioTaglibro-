cdd2126118b2917d17e7d7f5aa48ab4aa37db68c
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2025-01-19 19:40:59 +0100

    recuperar entrada concreta

--- Routes/Entries.js
+++ Routes/Entries.js
@@ -4,27 +4,39 @@ const User = require('../Models/user');
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+
+//Recuperar todas las entradas del usuario
+router.get("/:username", async (req, res) => {
     try {
-        const entries = await Entrada.find({});
+        const { username } = req.params;
+        const entries = await Entrada.find({ autor_username: username });
         res.json(entries);
     } catch (error) {
         res.status(500).json({ message: "Error al recuperar las entradas", error: error.message });
     }
 });
 
+//Recuperar una entrada específica
+router.get("/:username/:id", async (req,res) => {
+    try{ //TODO: revisar esto para corregirlo
+        const {username, id} = req.params;ç
+        const entries = await Entrada.find({_id: id})
+    } catch(error){
+        res.status(500).json({message: "Error al recuperar la entrada",error: error.message})
+    }
+});
 // Crear una nueva entrada
 router.post("/new", async (req, res) => {
-    const { titulo, contenido, autor_id } = req.body;
+    const { titulo, contenido, autor_username } = req.body;
 
     // Validar los datos recibidos
-    if (!titulo || !contenido || !autor_id) {
-        return res.status(400).json({ message: "Título, contenido y autor_id son requeridos." });
+    if (!titulo || !contenido || !autor_username) {
+        return res.status(400).json({ message: "Título, contenido y autor_username son requeridos." });
     }
 
     // Verificar si el autor existe
     try {
-        const usuarioExistente = await User.findById(autor_id);
+        const usuarioExistente = await User.findOne({ username: autor_username });
         if (!usuarioExistente) {
             return res.status(404).json({ message: "El usuario no existe." });
         }
@@ -32,7 +44,7 @@ router.post("/new", async (req, res) => {
         const nuevaEntrada = new Entrada({
             titulo,
             contenido,
-            autor_id,  // Usar ObjectId aquí para referenciar al usuario existente
+            autor_username,
         });
 
         const savedEntry = await nuevaEntrada.save();
@@ -42,5 +54,6 @@ router.post("/new", async (req, res) => {
         res.status(500).json({ message: "Error al guardar la entrada", error: error.message });
     }
 });
+
 
 module.exports = router;
