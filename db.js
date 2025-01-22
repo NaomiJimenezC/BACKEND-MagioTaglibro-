a30157043d7a353b5ac826e7344f04e12c95450f
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2025-01-22 13:06:40 +0100

    uri fixed

--- db.js
+++ db.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose');
 const account = process.env.MONGO_DB_ADMIN_USERNAME;
 const password = process.env.MONGO_DB_ADMIN_PASSWORD;
-const uri = `mongodb+srv://${account}:${password}.9ebei.mongodb.net/mydatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${account}:${password}@cluster0.9ebei.mongodb.net/mydatabase?retryWrites=true&w=majority`;
+
 mongoose.connect(uri)
   .then(() => console.log('Conexión a MongoDB exitosa'))
   .catch(err => console.error('Error al conectar con la base de datos:', err));
