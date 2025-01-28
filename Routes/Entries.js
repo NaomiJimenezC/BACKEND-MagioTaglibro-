379791ddf95f5c86aaca8dd603a2edcf83c982e9
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2025-01-28 19:37:36 +0100

    fixed last endpoint

--- Routes/Entries.js
+++ Routes/Entries.js
@@ -55,10 +55,6 @@ router.get("/shared-entries/:username/:id_entry", async(req,res)=>{
     try {
     const {username,id_entry} = req.params;
 
-    if (req.user.username !== username) {
-        return res.status(403).json({ message: "No autorizado" });
-    }
-
     const entry = await Entrada.findOne({
         _id: id_entry,
         compartido_con: { $in: [username] }
