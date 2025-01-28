d2eb4096fb623a41d0fac9c5366c63f0d5ce1a2e
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2025-01-28 11:58:04 +0100

    contact routes added to Server.js

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const authRoutes = require('./Routes/Auth');
 const entriesRoutes = require('./Routes/Entries');
 const friendshipRoutes = require('./Routes/Friendships')
+const contactRoutes = require('./Routes/Contact')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -17,6 +18,7 @@ app.use(cors());
 app.use('/api/entries',entriesRoutes);
 app.use('/api/users', authRoutes);
 app.use('/api/friendship',friendshipRoutes)
+app.use('/api/contact',contactRoutes)
 
 // Inicia el servidor
 app.listen(PORT, () => {
