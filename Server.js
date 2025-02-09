3055da149c63bc51f655ae943216ac3e26ed6302
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2025-02-09 19:18:25 +0100

    añadido el sitemap en react

--- Server.js
+++ Server.js
@@ -7,6 +7,7 @@ const contactRoutes = require('./Routes/Contact')
 const userRoutes = require('./Routes/UserEditor')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const {SitemapStream, streamToPromise} = require("sitemap");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -21,6 +22,33 @@ app.use('/api/users', authRoutes);
 app.use('/api/friendship',friendshipRoutes)
 app.use('/api/contact',contactRoutes)
 app.use('/api/userEditor',userRoutes)
+
+app.get('/sitemap.xml', async (req, res) => {
+  const baseUrl = 'https://front-magiotaglibro.onrender.com';
+  const today = new Date().toISOString();
+
+  const sitemapStream = new SitemapStream({ hostname: baseUrl });
+
+  const urls = [
+    { url: '/', lastmod: today, priority: 1.0 },
+    { url: '/contact', lastmod: today, priority: 0.8 },
+    { url: '/user', lastmod: today, priority: 0.8 },
+    { url: '/diaries', lastmod: today, priority: 0.6 },
+  ];
+
+  // Enviar el sitemap como XML
+  try {
+    urls.forEach(url => sitemapStream.write(url));
+    sitemapStream.end();
+
+    const sitemapXML = await streamToPromise(sitemapStream);
+    res.header('Content-Type', 'application/xml');
+    res.send(sitemapXML);
+  } catch (err) {
+    console.error("Error generating sitemap:", err);
+    res.status(500).send("Error generating sitemap");
+  }
+});
 
 // Inicia el servidor
 app.listen(PORT, () => {
